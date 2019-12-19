6648992c5335b63e8d53fd9e032cca52a6fb689b
AndreyMad <andrey.mad.87@gmail.com>
2019-12-19 23:58:53 +0200

    some commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import UserProfile from './UserProfile/UserProfile.jsx';
+import UserProfile from './UserProfile/UserProfile';
 import user from '../db/user.json';
-import UserFriends from './UserFriends/UserFriends.jsx';
+import UserFriends from './UserFriends/UserFriends';
 import friends from '../db/friends.json';
+import Stats from './Stats/Stats';
+import statisticalData from '../db/statistical-data.json';
+
 function App() {
   return (
     <>
       <UserProfile user={user} />
       <UserFriends friends={friends} />
+      <Stats title Stats={statisticalData} />
     </>
   );
 }
