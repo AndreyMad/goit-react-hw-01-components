cca5469ead845d260d1d87376ab6d00b5b1d6d24
AndreyMad <andrey.mad.87@gmail.com>
2019-12-21 22:25:18 +0200

    Updates

---
+++ precache-manifest.058634b8183d1955b05138742f6600a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddf1ad73b45f03d78f50d943086ee46",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "f3191e672f6a5c5aad85",
+    "url": "/goit-react-hw-01-components/static/css/main.857c60aa.chunk.css"
+  },
+  {
+    "revision": "6008a77ab4673933271a",
+    "url": "/goit-react-hw-01-components/static/js/2.66ab604d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.66ab604d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3191e672f6a5c5aad85",
+    "url": "/goit-react-hw-01-components/static/js/main.5769c379.chunk.js"
+  },
+  {
+    "revision": "351ec24c9bd9afb33e31",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.b5c70b18.js"
+  }
+]);
